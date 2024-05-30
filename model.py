09edff58d05da2d54e50d0cb7dab366d31aff083
SomeArbitraryMathMajor <bzang19@outlook.com>
2024-05-30 00:01:07 -0400

    adding comments

--- model.py
+++ model.py
@@ -47,6 +47,10 @@ def AppAction(tick):
     y_pred_df.index = test.index
     y_pred_out = y_pred_df["Predictions"]
 
+    '''
+    Testing out with NN -- see nn-test file
+    '''
+    
     ## Creating Figure
     fig = plt.gcf()
     plt.plot(train.index, train['Close'], color='black', label='Training')
